45a4bf1e4db9373e71b2ac8f39d0a3dab4221d55
xpan2 <xpan@walmartlabs.com>
2016-10-23 20:30:46 -0700

    Clock component

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -2,6 +2,12 @@ var React = require('react');
 
 
 var Clock = React.createClass({
+  getDefaultProps: function () {
+    totalSeconds: 0
+  },
+  propTypes: {
+    totalSeconds: React.PropTypes.number
+  },
   formatSeconds: function (totalSeconds) {
     var seconds = totalSeconds % 60;
     var minutes = Math.floor(totalSeconds / 60);
@@ -16,9 +22,12 @@ var Clock = React.createClass({
   },
 
   render: function () {
+    var {totalSeconds} = this.props;
     return (
-      <div>
-
+      <div className="clock">
+        <span className="clock-text">
+          {this.formatSeconds(totalSeconds)}
+        </span>
       </div>
     );
   }
--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,10 +1,11 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var Countdown = React.createClass({
   render: function () {
     return (
       <div>
-        <p>This is Countdown</p>
+        <Clock totalSeconds={129}/>
       </div>
     );
   }
