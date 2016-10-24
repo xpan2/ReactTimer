5e1b54f8a374ac77bff56b2dfee956c4ef84250b
xpan2 <xpan@walmartlabs.com>
2016-10-24 12:34:09 -0700

    add page title and tests

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -19,7 +19,6 @@ var Main = (props) => {
       <Nav/>
       <div className="row">
         <div className="column small-centered medium-6 large-4">
-          <p>Main.jsx Rendered</p>
           {props.children}
         </div>
       </div>
